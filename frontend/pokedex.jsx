f91e1edea3e3bb5e7f3f7a5c880afb4d0f6f8e3c
jiangtt18 <tjiang90111@gmail.com>
2018-08-18 23:40:02 -0700

    fixed

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -5,9 +5,11 @@ import {receiveAllPokemon} from './actions/pokemon_actions.js';
 import {fetchAllPokemon} from './util/api_util';
 import configureStore from './store/store';
 
+
 document.addEventListener("DOMContentLoaded", ()=>{
   const root = document.getElementById('root');
   const store = configureStore();
+  window.store = store;
   window.receiveAllPokemon = receiveAllPokemon;
   window.fetchAllPokemon = fetchAllPokemon;
   window.getState = store.getState;
--- frontend/reducers/pokemon_reducer.js
+++ frontend/reducers/pokemon_reducer.js
@@ -1,6 +1,6 @@
 import {RECEIVE_ALL_POKEMON} from '../actions/pokemon_actions';
 
-export const pokemonReducer = (oldState,action) => {
+export const pokemonReducer = (oldState={},action) => {
   Object.freeze(oldState);
   switch(action.type){
     case RECEIVE_ALL_POKEMON:
